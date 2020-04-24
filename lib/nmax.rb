0eb8801380544627cf1ed16cb680bf318ad2ea22
Mikhail Grigoriev <mgrigoriev@gmail.com>
2020-04-25 01:25:42 +0300

    Optimize runtime and memory usage by reading in chunks

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,17 +1,34 @@
-# frozen_string_literal: true
-
 require 'nmax/version'
 
 module Nmax
+  CHUNK_SIZE_IN_BYTES = 1024
+  UNIQALIZATION_FREQUENCY_IN_CHUNKS = 10_000
+
   class NotPositiveIntegerArgumentError < StandardError; end
 
   def self.find(n)
-    raise NotPositiveIntegerArgumentError if !n.is_a?(Integer) || !n.positive?
+    raise NotPositiveIntegerArgumentError unless n.is_a?(Integer) && n.positive?
 
     numbers = []
+    buffer = ''
+    chunk = ''
+    chunks_counter = 0
 
-    $stdin.each_line do |line|
-      line.scan(/\d+/) { |number| numbers << number.to_i }
+    until chunk.nil?
+      # Optimize for the scenario when we have a huge string and reading line by line won't work
+      chunk = $stdin.read CHUNK_SIZE_IN_BYTES
+      chunks_counter += 1
+
+      # Optimize runtime and memory usage
+      numbers.uniq! if (chunks_counter % UNIQALIZATION_FREQUENCY_IN_CHUNKS).zero?
+
+      unless chunk.nil?
+        buffer << chunk
+        next if buffer[-1] =~ /\d/
+      end
+
+      buffer.scan(/\d+/) { |number| numbers << number.to_i }
+      buffer = ''
     end
 
     numbers.uniq!
