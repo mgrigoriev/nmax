8c0886555121782e7a1011cb2fb14d0fc1c07bd2
Mikhail Grigoriev <mgrigoriev@gmail.com>
2020-04-24 03:23:59 +0300

    Implement Nmax.find method

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,6 +1,19 @@
 require "nmax/version"
 
 module Nmax
-  class Error < StandardError; end
-  # Your code goes here...
+  class NotPositiveArgumentError < StandardError; end
+
+  def self.find(n)
+    raise NotPositiveArgumentError unless n.positive?
+
+    numbers = []
+
+    $stdin.each_line do |line|
+      line.scan(/\d+/) { |number| numbers << number.to_i }
+    end
+
+    numbers.uniq!
+    numbers.sort!.reverse!
+    numbers[0...n]
+  end
 end
